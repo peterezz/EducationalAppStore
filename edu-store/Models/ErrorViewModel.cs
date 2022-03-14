1d0fe0861455509d3407cf8015c7415f1bd8659a
peterezzat <55260283+peterezz@users.noreply.github.com>
2022-03-14 23:15:48 +0200

    edu-store creation

---
+++ edu-store/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace edu_store.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
